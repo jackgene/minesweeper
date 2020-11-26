d307fce54a63d8f1e7e3f51db45d4e70c8d594ab
Jack Leow <jackgene@gmail.com>
2020-11-26 15:33:00 -0800

    Initial Commit

---
+++ Minesweeper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Minesweeper
+//
+//  Created by Jack Leow on 11/26/20.
+//  Copyright © 2020 Jack Leow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Minesweeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by Jack Leow on 11/26/20.
+//  Copyright © 2020 Jack Leow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
