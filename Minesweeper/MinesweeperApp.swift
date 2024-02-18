0c282b8000d097a17bde4b85b0389d9aca22ac05
Jack Leow <jackgene@gmail.com>
2024-02-18 06:55:59 -0800

    Empty Xcode project.

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Jack Leow on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
