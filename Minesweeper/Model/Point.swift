afd6f13a909a44e90cc2f8bc0a724f6770ba3d23
Jack Leow <jackgene@gmail.com>
2024-02-25 16:46:49 -0800

    Fixed bug in adjacent points calculation.

--- Minesweeper/Model/Point.swift
+++ Minesweeper/Model/Point.swift
@@ -16,17 +16,19 @@ struct Point: Equatable, Hashable {
             return []
         }
         
+        let maxLeft: UInt = size.width + 1
+        let maxTop: UInt = size.height + 1
         let adjLeftFrom: UInt = left > 0 ? left - 1 : 0
-        let adjLeftTo: UInt = left < size.width ? left + 1 : size.width
+        let adjLeftTo: UInt = left < maxLeft ? left + 1 : maxLeft
         let adjTopFrom: UInt = top > 0 ? top - 1 : 0
-        let adjTopTo: UInt = top < size.height ? top + 1 : size.height
+        let adjTopTo: UInt = top < maxTop ? top + 1 : maxTop
         
-        return (adjLeftFrom..<adjLeftTo).flatMap { (adjLeft: UInt) in
-            (adjTopFrom..<adjTopTo).compactMap { (adjTop: UInt) in
-                if top != adjTop && left != adjLeft {
-                    Point(left: adjLeft, top: adjTop)
-                } else {
+        return (adjLeftFrom...adjLeftTo).flatMap { (adjLeft: UInt) in
+            (adjTopFrom...adjTopTo).compactMap { (adjTop: UInt) in
+                if top == adjTop && left == adjLeft {
                     nil
+                } else {
+                    Point(left: adjLeft, top: adjTop)
                 }
             }
         }
