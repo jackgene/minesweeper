1d706b304edb01ebeca60f4c52b7f75d6c827ae8
Jack Leow <jackgene@gmail.com>
2024-02-27 20:48:33 -0800

    Added support for menu items.

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -5,133 +5,21 @@
 //  Created by Jack Leow on 2/18/24.
 //
 
-import Combine
 import SwiftUI
 
-fileprivate let defaultLevel: Level = Level(
-    size: Size(width: 9, height: 9), mines: 10
-)
-
-class ViewModel: ObservableObject {
-    @Published var inputs: Input = .startOver(level: defaultLevel)
-    @Published var cells: [[String?]] = []
-    @Published var resetable: Bool = false
-    
-    init() {
-        let mineFields: some Publisher<MineField, Never> = $inputs
-            .scan(.uninitialized(level: defaultLevel)) { (lastMineField: MineField, input: Input) in
-                let nextMineField: MineField = input.update(mineField: lastMineField)
-                return nextMineField
-            }
-            .share()
-        
-        mineFields
-            .map {
-                switch $0 {
-                case .uninitialized(level: let level):
-                    (0..<level.size.height).map { _ in
-                        (0..<level.size.width).map { _ in
-                            nil
-                        }
-                    }
-                    
-                case .sweeping(cells: let cells, remaining: let remaining):
-                    cells.enumerated().map { (top: Int, cells: [Cell]) in
-                        cells.enumerated().map { (left: Int, cell: Cell) in
-                            if remaining.contains(Point(left: UInt(left), top: UInt(top))) {
-                                nil
-                            } else {
-                                switch cell {
-                                case .empty(adjacentMines: let count):
-                                    count > 0 ? "\(count)" : ""
-                                case .mine:
-                                    nil
-                                }
-                            }
-                        }
-                    }
-                    
-                case .tripped(cells: let cells, mine: let mine):
-                    cells.enumerated().map { (top: Int, cells: [Cell]) in
-                        cells.enumerated().map { (left: Int, cell: Cell) in
-                            switch cell {
-                            case .empty(adjacentMines: let count):
-                                count > 0 ? "\(count)" : ""
-                            case .mine:
-                                if Point(left: UInt(left), top: UInt(top)) == mine {
-                                    "💥"
-                                } else {
-                                    "💣"
-                                }
-                            }
-                        }
-                    }
-
-                case .swept(cells: let cells):
-                    cells.map { (cells: [Cell]) in
-                        cells.map { (cell: Cell) in
-                            switch cell {
-                            case .empty(adjacentMines: let count):
-                                count > 0 ? "\(count)" : ""
-                            case .mine:
-                                "🚩"
-                            }
-                        }
-                    }
-                }
-            }
-            .assign(to: &$cells)
-        
-        mineFields
-            .map {
-                switch $0 {
-                case .uninitialized(_): false
-                case .sweeping(_, _), .tripped(_, _), .swept(_): true
-                }
-            }
-            .assign(to: &$resetable)
-//        mineFields
-//            .map {
-//                return switch $0 {
-//                case .uninitialized(level: let level):
-//                    level.size
-//                case .sweeping(cells: let cells, _),
-//                        .tripped(cells: let cells, _),
-//                        .swept(cells: let cells):
-//                    cells.size
-//                }
-//            }
-//            .removeDuplicates { $0 != $1 }
-//            .map { (size: Size) in
-//                (0..<Int(size.height)).map { (row: Int) in
-//                    (0..<Int(size.width)).map { (col: Int) in
-//                        mineFields.map {
-//                            switch $0 {
-//                            case .uninitialized(_):
-//                                nil
-//                            case .sweeping(cells: _, remaining: _):
-//                                "!"
-//                            case .tripped(cells: _, mine: _):
-//                                "💥"
-//                            case .swept(cells: _):
-//                                "🧹"
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            .assign(to: &$cells)
-    }
-}
-
 struct ContentView: View {
-    @StateObject var viewModel: ViewModel = ViewModel()
+    @ObservedObject
+    private var appState: AppState
+    
+    init(appState: AppState) {
+        self.appState = appState
+    }
     
     var body: some View {
         VStack(spacing: 20) {
             Grid {
                 ForEach(
-                    Array(viewModel.cells.enumerated()), id: \.offset
+                    Array(appState.cells.enumerated()), id: \.offset
                 ) { (top: Int, labels: [String?]) in
                     
                     GridRow {
@@ -141,7 +29,7 @@ struct ContentView: View {
                             
                             Button(
                                 action: {
-                                    viewModel.inputs = .visit(
+                                    appState.inputs = .visit(
                                         position: Point(
                                             left: UInt(left),
                                             top: UInt(top)
@@ -163,17 +51,17 @@ struct ContentView: View {
             
             Button(
                 action: {
-                    viewModel.inputs = .startOver(level: defaultLevel)
+                    appState.inputs = .startOver(level: defaultLevel)
                 }
             ) {
                 Text("New Game").font(.title2)
             }
-            .disabled(!viewModel.resetable)
+            .disabled(!appState.resetable)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(appState: AppState())
 }
--- Minesweeper/MinesweeperApp.swift
+++ Minesweeper/MinesweeperApp.swift
@@ -7,11 +7,38 @@
 
 import SwiftUI
 
+@objc
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var token: NSKeyValueObservation?
+    
+    func applicationShouldTerminateAfterLastWindowClosed(
+        _ sender: NSApplication
+    ) -> Bool {
+        return true
+    }
+}
+
 @main
 struct MinesweeperApp: App {
+    @StateObject
+    private var appState: AppState = AppState()
+    @NSApplicationDelegateAdaptor(AppDelegate.self)
+    private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }.windowResizability(.contentSize)
+            ContentView(appState: appState)
+        }
+        .commandsRemoved()
+        .commands {
+            CommandMenu("Game") {
+                Button("New") {
+                    appState.inputs = .startOver(level: defaultLevel)
+                }
+                .keyboardShortcut("N")
+                .disabled(!appState.resetable)
+            }
+        }
+        .windowResizability(.contentSize)
     }
 }
